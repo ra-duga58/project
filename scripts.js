dace88e6cc1f0d9d87c9bb58aaec3a9555315246
ra-duga58 <ra-duga58@mail.ru>
2019-09-09 14:23:20 +0300

    add true script

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
-let money = prompt('Ваш бюдже за месяц?'),
-	time = prompt('Введите дату в формате YYYY-MM-DD');
-let expenses = {};
-let optionalExpenses = {};
-let income = [];
-let costs = prompt('Введите обязательную статью расходов в этом месяце'),
-	price = prompt('Во сколько обойдется');
-let appData = {money, time, expenses, income, saving: false};
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-expenses = {'costs': 'price'};
-let budget = money/30;
-alert(budget);
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
