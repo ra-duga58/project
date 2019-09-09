a2068cf194aba074c8476aa9d3424c6d7066a2bf
ra-duga58 <ra-duga58@mail.ru>
2019-09-09 14:04:48 +0300

    add js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-console.log(d);
+let money = prompt('Ваш бюдже за месяц?'),
+	time = prompt('Введите дату в формате YYYY-MM-DD');
+let expenses = {};
+let optionalExpenses = {};
+let income = [];
+let costs = prompt('Введите обязательную статью расходов в этом месяце'),
+	price = prompt('Во сколько обойдется');
+let appData = {money, time, expenses, income, saving: false};
+
+expenses = {'costs': 'price'};
+let budget = money/30;
+alert(budget);
